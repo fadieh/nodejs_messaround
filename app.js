8bbf25c0bc42314c10a8849992ddbc18a7f3d895
Fadie Hannona <fadiehannona@gmail.com>
2014-11-10 16:50:28 +0000

    stylesheets, views and server updated

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use(express.static(__dirname + '/public'))
 // Creating route
 
 app.get('/', function (req, res) {
-  res.end('Hi there!')
+  res.render('index',
+  { title: 'Home' }
+  )
 })
+
 app.listen(3000)
