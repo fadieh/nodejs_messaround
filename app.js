a9d7e0ad81b79f311a9c2459e77dd7607b9ccf93
Fadie Hannona <fadiehannona@gmail.com>
2014-11-10 16:33:00 +0000

    Fixed server

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// MODULE DEPENDENCIES
+
 var express = require('express')
 var stylus = require('stylus')
 var nib = require('nib')
+
+// TELL TO USE JADE AND STYLUS AND OTHERS
 
 var app = express()
 function compile(str, path) {
@@ -8,12 +12,26 @@ function compile(str, path) {
   .set('filename', path)
   .use(nib())
 }
+
+// TELL EXPRESS TO USE JADE
+// AND WHERE TO KEEP VIEWS
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
+
+// PASS MIDDLEWARE FUNCTIONS FOR EXPRESS TO USE.
+
 app.use(express.logger('dev'))
-app.use(stylus.middle(
+app.use(stylus.middleware(
   { src: __dirname + '/public'
   , compile: compile
   }
 ))
-app.use(express.static(__dirnam + '/public'))
+app.use(express.static(__dirname + '/public'))
+
+// Creating route
+
+app.get('/', function (req, res) {
+  res.end('Hi there!')
+})
+app.listen(3000)
